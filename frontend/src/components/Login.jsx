d2d01675edafe46d9ba908bcec6208f0bdc286ca
RichardWMuller <richardwmuller@gmail.com>
2025-08-18 15:53:34 -0300

    New Update

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,33 +1,45 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { authorize } from '../utils/auth'
+import Popup from './Popup'
+import InfoTooltip from '../components/InfoTooltip'
 
 function Login({ setIsLoggedIn, setUserEmail }) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [hasSubmitSucceeded, setHasSubmitSucceeded] = useState(null)
   const navigate = useNavigate()
 
   async function handleSubmit(e) {
     e.preventDefault()
-    setErrorMessage('') // limpa erro anterior
+    setHasSubmitSucceeded(null) // limpa estado anterior
 
     try {
       const credentials = { email, password }
-      const data = await authorize(credentials)
+      const response = await authorize(credentials)
 
-      if (!data?.token) {
-        throw new Error(`Token inválido: ${JSON.stringify(data)}`)
+      if (!response.ok || !response.token) {
+        throw new Error('Falha no login. Por favor, verifique suas credenciais.')
       }
 
-      localStorage.setItem('jwt', data.token)
+      localStorage.setItem('jwt', response.token)
       localStorage.setItem('userEmail', email)
       setUserEmail(email)
       setIsLoggedIn(true)
-      navigate('/')
+      setHasSubmitSucceeded(true)
+      setIsModalOpen(true)
     } catch (error) {
-      setErrorMessage(error)
       console.log('ERROR - LOGIN:', error)
+      setHasSubmitSucceeded(false)
+      setIsModalOpen(true)
+    }
+  }
+
+  function handleCloseModal() {
+    setIsModalOpen(false)
+    if (hasSubmitSucceeded) {
+      navigate('/') // navega só se sucesso
     }
   }
 
@@ -35,11 +47,9 @@ function Login({ setIsLoggedIn, setUserEmail }) {
     <div className="page">
       <h2 className="form__title">Entrar</h2>
 
-      {errorMessage && (
-        <div className="error-popup">
-          {errorMessage}
-        </div>
-      )}
+      <Popup isOpen={isModalOpen} onClosePopup={handleCloseModal} readOnly>
+        {hasSubmitSucceeded !== null && <InfoTooltip state={hasSubmitSucceeded} />}
+      </Popup>
 
       <form className="form__content" onSubmit={handleSubmit}>
         <input
